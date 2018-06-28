06f39b2ca207d5396ca5b0722b406e546ab29e03
serhat çetinkaya <serhatcetinkayaa@gmail.com>
2018-06-28 14:38:25 +0300

    better logging

--- remote-job.sh
+++ remote-job.sh
@@ -2,13 +2,17 @@
 ###
 # Trigger a Remote Jenkins Job with parameters and get console output as well as result
 # Usage:
-# remote-job.sh -u https://jenkins-url.com -j JOB_NAME -p "PARAM1=999" -p "PARAM2=123" -t BUILD_TOKEN
+# remote-job.sh -u https://jenkins-url.com -j JOB_NAME -t BUILD_TOKEN -s JENKINS_USER -r API_TOKEN -p "PARAM1=999" -p "PARAM2=123"
 # -u: url of jenkins host
 # -j: JOB_NAME on jenkins host
 # -p: parameter to pass in. Send multiple parameters by passing in multiple -p flags
 # -t: BUILD_TOKEN on remote machine to run job
+# -s: Jenkins user on remote machine to authenticate
+# -r: Jenkins user API token on remote machine to authenticate
 # -i: Tell curl to ignore cert validation
 ###
+
+set -xe
 
 # Number of seconds before timing out
 [ -z "$BUILD_TIMEOUT_SECONDS" ] && BUILD_TIMEOUT_SECONDS=3600
@@ -28,31 +32,31 @@ while getopts j:p:t:u:s:r:i opt; do
 done
 shift $((OPTIND -1))
 
-[ -z "$JENKINS_URL" ] && { echo "JENKINS_URL (-u) not set"; exit 1; }
-echo "[INFO] JENKINS_URL: $JENKINS_URL"
-[ -z "$JOB_NAME" ] && { echo "JOB_NAME (-j) not set"; exit 1; }
-echo "[INFO] JOB_NAME: $JOB_NAME"
+[ -z "$JENKINS_URL" ] && { logger -s "[ERROR] $(date) JENKINS_URL (-u) not set"; exit 1; }
+logger -s "[INFO] $(date) JENKINS_URL: $JENKINS_URL"
+[ -z "$JOB_NAME" ] && { logger -s "[ERROR] $(date) JOB_NAME (-j) not set"; exit 1; }
+logger -s "[INFO] $(date) JOB_NAME: $JOB_NAME"
 
-echo "The whole list of values is '${parameters[@]}'"
+logger -s "[INFO] $(date) The whole list of values is '${parameters[@]}'"
 for parameter in "${parameters[@]}"; do
   # If PARAMS exists, add an ampersand
   [ -n "$PARAMS" ] && PARAMS=$PARAMS\&$parameter
   # If no PARAMS exist, don't add an ampersand
   [ -z "$PARAMS" ] && PARAMS=$parameter
 done
-[ -z "$PARAMS" ] && { echo "[ERROR] No parameters were set!"; exit 1; }
-echo "[INFO] PARAMS: $PARAMS"
+[ -z "$PARAMS" ] && { logger -s "[ERROR] $(date) No parameters were set!"; exit 1; }
+logger -s "[INFO] $(date) PARAMS: $PARAMS"
 
 # Queue up the job
 # nb You must use the buildWithParameters build invocation as this
 # is the only mechanism of receiving the "Queued" job id (via HTTP Location header)
 
 REMOTE_JOB_URL="$JENKINS_URL/job/$JOB_NAME/buildWithParameters?$PARAMS"
-echo "[INFO] Calling REMOTE_JOB_URL: $REMOTE_JOB_URL"
+logger -s "[INFO] $(date) Calling REMOTE_JOB_URL: $REMOTE_JOB_URL"
 
 QUEUED_URL=$(curl -XPOST -sSL --user $JENKINS_USER:$API_TOKEN $CURL_OPTS -D - "$REMOTE_JOB_URL" | grep Location | awk {'print $2'})
 #perl -n -e '/^Location: (.*)$/ && print "$1\n"')
-[ -z "$QUEUED_URL" ] && { echo "[ERROR] No QUEUED_URL was found.  Did you remember to set a token (-t)?"; exit 1; }
+[ -z "$QUEUED_URL" ] && { logger -s "[ERROR] $(date) No QUEUED_URL was found.  Did you remember to set a token (-t)?"; exit 1; }
 
 # Remove extra \r at end, add /api/json path
 QUEUED_URL=${QUEUED_URL%$'\r'}api/json
@@ -63,7 +67,7 @@ JOB_URL=`curl -XPOST -sSL --user $JENKINS_USER:$API_TOKEN $QUEUED_URL | jq -r '.
 # Check for status of queued job, whether it is running yet
 COUNTER=0
 while [ -z "$JOB_URL" ]; do
-  echo "[INFO] The QUEUED counter is $COUNTER"
+  logger -s "[INFO] $(date) The QUEUED counter is $COUNTER"
   let COUNTER=COUNTER+$POLL_INTERVAL
   sleep $POLL_INTERVAL
   if [ "$COUNTER" -gt $BUILD_TIMEOUT_SECONDS ];
@@ -73,7 +77,7 @@ while [ -z "$JOB_URL" ]; do
   JOB_URL=`curl -XPOST -sSL --user $JENKINS_USER:$API_TOKEN $CURL_OPTS $QUEUED_URL | jq -r '.executable.url'`
   [ "$JOB_URL" = "null" ] && unset JOB_URL
 done
-echo "[INFO] JOB_URL: $JOB_URL"
+logger -s "[INFO] $(date) JOB_URL: $JOB_URL"
 
 # Job is running
 IS_BUILDING="true"
@@ -88,7 +92,7 @@ until [ "$IS_BUILDING" = "false" ]; do
   sleep $POLL_INTERVAL
   if [ "$COUNTER" -gt $BUILD_TIMEOUT_SECONDS ];
   then
-    echo "[ERROR] TIME-OUT: Exceeded $BUILD_TIMEOUT_SECONDS seconds"
+    logger -s "[ERROR] $(date) TIME-OUT: Exceeded $BUILD_TIMEOUT_SECONDS seconds"
     break  # Skip entire rest of loop.
   fi
   IS_BUILDING=`curl -XPOST -sSL --user $JENKINS_USER:$API_TOKEN $CURL_OPTS $JOB_URL/api/json | jq -r '.building'`
@@ -106,9 +110,9 @@ done
 RESULT=`curl -XPOST -sSL --user $JENKINS_USER:$API_TOKEN $CURL_OPTS $JOB_URL/api/json | jq -r '.result'`
 if [ "$RESULT" = 'SUCCESS' ]
 then
-  echo "[INFO] BUILD RESULT: $RESULT"
+  logger -s "[INFO] $(date) BUILD RESULT: $RESULT"
   exit 0
 else
-  echo "[ERROR] BUILD RESULT: $RESULT - Build is unsuccessful, timed out, or status could not be obtained."
+  logger -s "[ERROR] $(date) BUILD RESULT: $RESULT - Build is unsuccessful, timed out, or status could not be obtained."
   exit 1
 fi
